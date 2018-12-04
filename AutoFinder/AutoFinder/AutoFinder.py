b51f86e81bf0ec5a3999efb2d15465ab5165e89a
Mateusz Bieńkowski <bi3niol@gmail.com>
2018-12-04 23:31:07 +0100

    initial

--- AutoFinder/AutoFinder/AutoFinder.py
+++ AutoFinder/AutoFinder/AutoFinder.py
@@ -1,1 +1,4 @@
-
+import io
+if __name__ == "__main__":
+    
+    print("Helo")
---
+++ AutoFinder/AutoFinder/DataManipulations/ImageToArrayConverter.py
@@ -0,0 +1,7 @@
+from scipy import misc
+import matplotlib.pyplot as plt
+
+def ImageToArrayConverter(imagePath):
+    image = misc.imread(imagePath)
+    plt.imshow(imaga)
+    print("test")
